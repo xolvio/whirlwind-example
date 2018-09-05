18a805641040920d6d80721819ea6f5d228041e3
Przemysław Gierski <pgierski@o2.pl>
2018-09-05 14:38:13 +0200

    fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cp serverless.yml ./node_modules/whirlwind/serverless.yml
-cp serverless.yml ./node_modules/whirlwind/serverless.yml
+cp scenarioProcessor.js ./node_modules/whirlwind/scenarioProcessor.js
 cd ./node_modules/whirlwind
 npm run deploy
 git reset --hard origin/master
